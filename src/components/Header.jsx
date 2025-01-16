98c49d82d9ec81f7587776d4081467a2658bf9aa
DESKTOP-NRUK56G\DELL <kawidirangalla@gmail.com>
2025-01-17 00:18:14 +0530

    'header'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,11 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex item-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id=''>
-      <Navbar/>
+    <div className='min-h-screen mb-4 bg-cover bg-center flex item-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
+     <Navbar/>
+     <div>
+       <h2>Unlock the doors to your dreams</h2> 
+      </div>
     </div>
   )
 }
